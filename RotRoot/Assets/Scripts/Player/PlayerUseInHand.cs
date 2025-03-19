befc4063fa6dbcb31395b94d3ca46d3c406c0426
Joseph Staria (RIT Student) <jws8554@rit.edu>
2025-03-19 13:44:43 -0400

    Made it at least playable for right now

--- RotRoot/Assets/Scripts/Player/PlayerUseInHand.cs
+++ RotRoot/Assets/Scripts/Player/PlayerUseInHand.cs
@@ -7,12 +7,14 @@ public class PlayerUseInHand : MonoBehaviour
 {
     [SerializeField] private Transform handTransform;
 
-    public Item ItemInHand { get => currentSlotUI.InventorySlotReference.GetComponent<Item>(); }
+    public Item ItemInHand { get => currentObj != null ? currentObj?.GetComponent<Item>() : null; }
     private GameObject currentObj;
     private SlotUI currentSlotUI;
 
     public void UseUpdate()
     {
+        if (ItemInHand == null) return;
+
         if (ItemInHand != null)
             ItemInHand.InUse = false;
         if (Input.GetMouseButton(1))
@@ -23,31 +25,51 @@ public class PlayerUseInHand : MonoBehaviour
 
     public void SetItemInHand(SlotUI slot)
     {
-        if (slot == currentSlotUI) return;
-        // Make this shit work!!
-        string name;
         if (currentObj != null)
-            name = currentObj.name;
-
-        if (currentSlotUI != null && currentObj != null)
         {
-            currentSlotUI.SetSlotObj(currentObj.GetComponent<Item>());
             Destroy(currentObj);
         }
 
-        currentSlotUI = slot;
+        if (slot != null)
+        {
+            currentSlotUI = slot;
 
-        if (slot.InventorySlotReference == null) return;
+            if (slot.InventorySlotReference != null)
+            {
+                string name = slot.InventorySlotReference.name;
 
-        GameObject obj = slot.InventorySlotReference.GetComponent<Item>().gameObject;
+                currentObj = Instantiate(slot.InventorySlotReference, handTransform);
+                currentObj.name = name;
+            }
+        }
 
-        name = obj?.name;
+        
 
-        if (obj == null) return;
-
-        currentObj = Instantiate(obj, handTransform);
-        currentObj.name = name;
-
-        slot.SetSlotObj(currentObj.GetComponent<Item>());  
+        //if (slot == currentSlotUI) return;
+        //// Make this shit work!!
+        //string name;
+        //if (currentObj != null)
+        //    name = currentObj.name;
+        //
+        //if (currentSlotUI != null && currentObj != null)
+        //{
+        //    currentSlotUI.SetSlotObj(currentObj.GetComponent<Item>());
+        //    Destroy(currentObj);
+        //}
+        //
+        //currentSlotUI = slot;
+        //
+        //if (slot.InventorySlotReference == null) return;
+        //
+        //GameObject obj = slot.InventorySlotReference.GetComponent<Item>().gameObject;
+        //
+        //name = obj?.name;
+        //
+        //if (obj == null) return;
+        //
+        //currentObj = Instantiate(obj, handTransform);
+        //currentObj.name = name;
+        //
+        //slot.SetSlotObj(currentObj.GetComponent<Item>());  
     }
 }
